712aec20bb98c1a2e28f34fa2bebe9cf83926ee1
Wendell Smith <wendell18cuenta@gmail.com>
2025-04-17 15:33:56 -0500

    Agregar axios como dependencia y corregir la ruta de importación de Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./css/bootstrap.min.css";
+import "./css/boostrap/bootstrap.min.css";
 import "./css/style.css";
 import { PrediccionApp } from "./PrediccionApp.jsx";
 import { store } from "./app/store.js";
